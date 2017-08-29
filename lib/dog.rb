fd2c5ce418f1d16fdb75ff0c8e89225edd6c84ff
Chris Nady <nady.christopher@gmail.com>
2017-08-29 00:55:05 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Dog
+
+  def initialize(full_name)
+    @name = full_name
+  end
+
+  def name=(full_name)
+    @name = full_name
+  end
+
+  def name
+    @name
+  end
+
+  def bark
+    puts"Woof!"
+  end
+
+end
